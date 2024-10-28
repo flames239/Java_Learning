d2461c55a40133e81dd9060ad59609e754890b51
Hà Nguyễn Phương Nam <73868730+flames239@users.noreply.github.com>
2024-10-28 14:10:05 +0700

    Clone project to Repository Java_Learning

---
+++ src/Java_Concept/Core_Java/First_Program.java
@@ -0,0 +1,18 @@
+package Java_Concept.Core_Java;
+
+public class First_Program {
+    /*
+    Create project
+    package in Java
+    user defined and inbuilt
+    execute via command line
+    */
+
+    public static void main(String[] args) {
+        System.out.print("this is print");System.out.println("this is println");
+        System.out.println("this is println");
+
+        System.out.print("this is print\n"); // (\n:xuống hàng)
+        System.out.println("this is println");
+    }
+}
---
+++ src/Java_Concept/Primitive_Data_Type/Different_approach_variable.java
@@ -0,0 +1,17 @@
+package Java_Concept.Primitive_Data_Type;
+
+public class Different_approach_variable {
+    public static void main(String[] args) {
+        // approach 1
+        int a;
+        a = 10;
+
+        // approach 2
+        int a1,b1; // declare multiple variable
+        a1=10;
+        b1=10;
+
+        // approach 3
+        int a2=10,b2=10; // declare multiple variable and value
+    }
+}
